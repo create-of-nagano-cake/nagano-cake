9461650c56f9c4fc6d894f724e3a2af3d4529bda
Kazuya Arita <135966181+kazuya-arita@users.noreply.github.com>
2023-09-01 14:07:53 +0000

    wakaranai

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,9 +1,11 @@
 class Public::CartItemsController < ApplicationController
   
   def index
+    @items = Item.all
   end
   
   def create
+    @item = Item.find(cart_item_params[:item_id])
   end  
   
   def update 
@@ -15,4 +17,9 @@ class Public::CartItemsController < ApplicationController
   def destroy_all
   end
   
+  private
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
 end
