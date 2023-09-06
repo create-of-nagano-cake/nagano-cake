5ff1d7dbd244e7415ce365c5b07e87bfdaf8a926
Kazuya Arita <135966181+kazuya-arita@users.noreply.github.com>
2023-09-06 13:19:41 +0000

    enum途中

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
+  
+  enum address: { current_customer: 0, address: 1, new_address: 2 }
+  
   belongs_to :customer
+  
+  def address_display
+    '〒' + postal_code + ' ' + address + '' + name
+  end  
+  
 end
