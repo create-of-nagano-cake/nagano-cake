d3023978b44aa6968d5f05b2f1f7be87b687b61e
Kazuya Arita <135966181+kazuya-arita@users.noreply.github.com>
2023-09-10 12:55:12 +0000

    管理者会員ごとの注文詳細実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,15 @@
 class Admin::OrdersController < ApplicationController
   
   def show
+    @order_details = OrderDetail.where(order_id: params[:id])
+    @order = Order.find(params[:id])
+    @total = 0
+  end
+  
+  private
+  
+  def order_params
+   params.require(:order).permit(:payment_method, :postal_code, :address, :name, :total_payment, :customer_id, :postage)
   end
   
 end
