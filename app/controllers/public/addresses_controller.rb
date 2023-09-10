5b75caf1f633e097517bdaf7d8dc053a5afad0f5
Kazuya Arita <135966181+kazuya-arita@users.noreply.github.com>
2023-09-10 07:11:48 +0000

    注文情報一部修正（モデル）

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,13 +6,13 @@ class Public::AddressesController < ApplicationController
     if @address.save
      redirect_to addresses_path
     else
-     @addresses = Address.all  
+     @addresses = current_customer.addresses
      render :index
     end   
   end
   
   def index
-    @addresses = Address.all
+    @addresses = current_customer.addresses
     @address = Address.new
   end
   
